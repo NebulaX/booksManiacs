d671b47cd062bb4b091308b6241aa57f37617e47
Akshesh <aksheshdoshi@gmail.com>
2013-12-22 05:27:50 +0530

    change models.py

--- website/booksManiacs/admin.py
+++ website/booksManiacs/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from booksManiacs.models import Book, Profile,User
+from booksManiacs.models import Student_Year, Book, Profile, User, Item
 
 admin.site.register(Book)
 admin.site.register(Profile)
-admin.site.register(User)
+admin.site.register(Item)
--- website/booksManiacs/models.py
+++ website/booksManiacs/models.py
@@ -2,12 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Book(models.Model):
-	name = models.CharField(max_length=100)
-	author = models.CharField(max_length=40)
-	edition = models.CharField(max_length=15)
-	def __unicode__(self):
-		return self.name
 
 class Student_Year(models.Model):
     FIRST = '1st'
@@ -21,17 +15,39 @@ class Student_Year(models.Model):
         (FOURTH, 'Fourth'),
     )
 
+
+class Book(models.Model):
+	name = models.CharField(max_length=100)
+	author = models.CharField(max_length=40,primary_key=True)
+	avail_count = models.IntegerField(default=0)
+	def __unicode__(self):
+		return "name - " + self.name + "author - " + self.author
+
 class Profile(models.Model):
 	name = models.CharField(max_length=50)
 	email = models.EmailField(max_length=254,primary_key=True)
-	phone_number = models.CharField(max_length=10)
+	mobile_number = models.CharField(max_length=10)
 	room_number = models.CharField(max_length=10)
-	year = models.CharField(max_length=3,choices=Student_Year.YEAR_CHOICES,default='')
+	year = models.CharField(max_length=3,choices=Student_Year.YEAR_CHOICES)
 	enrollment_number = models.CharField(max_length=8)
-	books = models.ManyToManyField(Book)
+	synced_facebook = models.BooleanField(default=False)
+	blocked = models.BooleanField(default=False)
 	def __unicode__(self):
-		return self.name
+		return self.email
 
 class User(models.Model):
 	email = models.OneToOneField(Profile)
 	password = models.CharField(max_length=32)
+
+class Item(models.Model):
+	ITEM_STATUS = (
+		('0','no_request'),
+		('1','request_to_buy'),
+		('2','sold'),
+	)
+	name = 	models.ForeignKey(Book)
+	edition = models.CharField(max_length=15)
+	seller = models.ForeignKey(Profile)
+	buy_request = models.CharField(max_length=1,choices=ITEM_STATUS,default='0')
+	buyer = models.ForeignKey(Profile,related_name='+',default='')
+	other_details = models.TextField(default='')
