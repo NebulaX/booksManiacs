44fcdd65d71eb1fca2945cb54deedbfaa565258b
Akshesh <aksheshdoshi@gmail.com>
2013-12-27 04:34:27 +0530

    modify html pages

--- website/booksManiacs/urls.py
+++ website/booksManiacs/urls.py
@@ -4,5 +4,6 @@ from booksManiacs import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.home, name='home'),
-	url(r'^books/', views.books, name='books')
+	url(r'^books/$', views.books, name='books'),
+	url(r'^items/(?P<book_author>[\w\s]+)/$', views.items, name='items'),
 )
--- website/booksManiacs/views.py
+++ website/booksManiacs/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 # from django.template import RequestContext, loader
 
-from booksManiacs.models import Book
+from booksManiacs.models import Book, Item
 
 def home(request):
     return render(request, 'booksManiacs/home.html')
@@ -13,3 +13,14 @@ def books(request):
 	books_available =  Book.objects.all()                 #books with avail_count!=0
 	data = {'books_available': books_available}
 	return render(request, 'booksManiacs/books.html', data)
+
+def items(request, book_author):
+	exist = Book.objects.filter(author=book_author).count()
+	if exist:
+		# return HttpResponse("<h1>It works! %s</h1>" % book_author)
+		req_items = Item.objects.filter(name=book_author)
+		data = {'req_items' : req_items, 'book_author' : book_author, 'exist' : exist}
+		return render(request, 'booksManiacs/items.html', data)
+	else:
+		return HttpResponse("sorry there is no such book. you have reached the wrong page.")
+		#404page
