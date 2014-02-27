bbd440a7f9a3ed98e52e09165609cfee451af475
Akshesh <aksheshdoshi@gmail.com>
2014-02-27 14:29:04 +0530

    solve sessions problem with different templates

--- website/website/settings.py
+++ website/website/settings.py
@@ -118,6 +118,11 @@ TEMPLATE_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.request',
+)
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
