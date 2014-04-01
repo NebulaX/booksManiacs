315542e90463fb6fb40617911196fa8a863ee255
Akshay Deshraj <akshay30nov@gmail.com>
2014-04-02 00:43:16 +0530

    Add frontend checks for signup form

--- website/booksManiacs/static/booksManiacs/js/application.js
+++ website/booksManiacs/static/booksManiacs/js/application.js
@@ -1,7 +1,38 @@
 $(document).ready(function (){
+	
 	$("#headerContent a").tooltip();
+
+$("#signup").submit(function (event)
+{
+	var a=$("#name");
+	if(!(a.val().match(/^[a-zA-Z ]+$/)))
+	{
+		alert("Please enter a valid name");
+		a.focus();
+		return false;
+	}
+	var a=$("#phone");
+	if(!(a.val().match(/^[789]\d{9}$/)))
+	{
+		alert("Please enter a valid phone number");
+		a.focus();
+		return false;
+	}
+	var a=$("#password");
+	var b=$("#confirm_password");
+	if( a.val()=="" ){
+		alert("Please enter a valid password");
+		a.focus();
+		return false;
+	}
+	if(!a.val()==b.val()){
+		alert("Password Mismatch !!");
+		a.focus();
+		return false;
+	}
 });
 
+});
 
 
 function coverUp(){
